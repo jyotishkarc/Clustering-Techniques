8ac063e207be3e1f827898c7409a33a87c937534
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-07-09 12:27:04 +0530

    Modified DP means code

--- dp-means.R
+++ dp-means.R
@@ -1,51 +1,39 @@
 
-DP.means <- function(X, lambda,epsilon){
+DP.means <- function(X, lambda, epsilon){
    
    n <- nrow(X)
-   K <- 1
-   mu <- t(colMeans(X))
-   z <- rep(1, n)
-   fold<-0
+   C <- 1
+   mu[1,] <- t(colMeans(X))
+   Z <- rep(1, n)
+   
+   fold <- sum((X - matrix(rep(mu[1,], n), n, ncol(X), byrow = TRUE))^2) + lambda
    
    while(TRUE)
    {
-      fnew<-0
-      
       for (i in 1:n)
       {
-         m <- matrix(rep(, ), 3, 4, byrow = TRUE)
-         m<-m%*%t(m)
-         if(min(diag(m))>lambda)
-         {
-            K<-K+1
-            Z[i]<-K
-            mu<-matrix(c(as.vector(t(m)),X[i,]),K,byrow<-T)
-         }
-         else
-            Z[i]<-which.min(diag(m))
+         for (h in 1:C) {dist.vec[h] <- sum((X[i,] - mu[h])^2)}
          
-         j<-1
-         while(j<=K)
+         if(min(dist.vec) > lambda)
          {
-            mu[j,]<-colMeans(X[Z==j,])
-            j<-j+1
+            Z[i] <- C <- C+1
+            mu[C,] <- t(X[i,])
          }
+         
+         else {Z[i] <- which.min(dist.vec)}
+         
+         for(j in 1:C) {mu[j,] <- colMeans(X[Z==j,])}
       }
       
-      R<-matrix(0,n,ncol(X))
-      j<-1
-      while(j<=K)
-      {
-         R[Z==j,]<-mu[j,]
-         j<-j+1
-      }
-      fnew<-sum(diag((X-R)%*%t(X-R)))
-      if(abs(fnew-fold)<epsilon)
-         break
-      fold<-fnew
+      
+      R <- matrix(0,n,ncol(X))
+      
+      for (j in 1:C) {R[Z==j,] <- mu[j,]}
+      
+      f.new <- sum((X-R)^2) + lambda * C
+      if((f.new - f.old)^2 < epsilon) {break}
+      f.old <- f.new
    }
-   return(list("z"<-Z,"m"<-mu))
    
-   
+   return(list("Z" = Z , "mu" = mu))
 }
-
