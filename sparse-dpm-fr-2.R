2b5ce503ede6488c6ae0819075ad523e925dcd63
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-08-21 00:11:42 +0530

    Added the code for DP-means on SKFR-2

--- sparse-dpm-fr-2.R
+++ sparse-dpm-fr-2.R
@@ -1,6 +1,6 @@
 ## Author : SUPRATIK BASU
 
-sparse.dpm.fr.2 <- function(X, s, lambda, tolerance = 1e-07)
+sparse.dpm.fr.2 <- function(X, s, lambda, gt = NULL, tolerance = 1e-05)
 {
   N <- nrow(X)
   d <- ncol(X)
@@ -17,33 +17,51 @@ sparse.dpm.fr.2 <- function(X, s, lambda, tolerance = 1e-07)
   while(TRUE)
   {
     D <- matrix(0, C, d)
-    rank <- matrix(0, C, s)
+    r <- matrix(0, C, d)
     obj.new <- lambda * C
-    
     for(j in 1:C)
     {
-      centroid[j, ] <- colMeans(X[which(Z[ ,j]==1), ])
+      
+      if(length(which(Z==j))==0)
+      {
+        centroid[j,] <- rep(0, d)
+      }
+      else if(length(which(Z==j))==1)
+      {
+        centroid[j,] <- X[which(Z==j), ]
+      }
+      else
+      {
+        centroid[j, ] <- colMeans(X[which(Z==j), ])
+      }
       for(l in 1:d)
       {
-        D[j,l] <- sum(Z[ ,j]) * centroid[j,l] ^ 2
+        D[j,l] <- centroid[j,l] ^ 2
       }
-      rank[j, ] <- order(D[j, ])
+      
+      r[j, ] <- (d+1)-rank(D[j, ])
     }
-    
     for(i in 1:N)
     {
       dist.mat <- rep(0, C)
       for(j in 1:C)
       {
-        dist.mat[j] <- sum((X[i, rank[j,1:s]] - centroid[j, rank[j,l]]) ^ 2) + 
-                                                    sum((X[i, rank[j,s+1:N]]) ^ 2)
+        u <- which(r[j, ] <= s)
+        if(s<d){
+        v <- which(r[j, ] > s)}
+        dist.mat[j] <- sum((X[i, r[j,u]] - centroid[j, r[j,u]]) ^ 2) 
+        if(s < d)
+        {
+          dist.mat[j] <- dist.mat[j] + sum((X[i, r[j,v]]) ^ 2)
+        }
       }
-      
       if(min(dist.mat) > lambda)
       {
         C <- C+1
         Z[i] <- C
-        centroid <- matrix(c(as.numeric(t(centroid)), X[i,]), nrow = C, byrow = TRUE)
+        centroid <- matrix(c(as.vector(t(centroid)), X[i,]), nrow = C, byrow = TRUE)
+        D <- matrix(c(as.vector(D),centroid[C,]^2),C,d,byrow=T)
+        r <- matrix(c(as.vector(r),rank(X[i,]^2)),C,d,byrow=T)
         obj.new <- obj.new + lambda
       }
       else
@@ -52,10 +70,18 @@ sparse.dpm.fr.2 <- function(X, s, lambda, tolerance = 1e-07)
         obj.new <- obj.new + min(dist.mat)
       }
     }
-    
-    if(abs(obj.new - obj.old) < tolerance) {break}
-    
+    if(abs(obj.new - obj.old) < tolerance)
+    {
+      break
+    }
     obj.old <- obj.new
+    print(C)
     t <- t+1
   }
+  print(C)
+  if(is.null(gt)==F)
+  {
+    print(NMI(Z, gt))
+  }
+  return(list("C"=C,"Z"=Z))
 }
