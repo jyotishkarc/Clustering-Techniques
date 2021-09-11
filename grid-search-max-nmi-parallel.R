6db97ebd828216668a8ed261051bb7173c2b5d5d
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-09-11 19:18:04 +0530

    Minor changes

--- grid-search-max-nmi-parallel.R
+++ grid-search-max-nmi-parallel.R
@@ -1,16 +1,16 @@
 
-start.time <- proc.time()
-
 library(doParallel)
 
-grid.search <- function(X, s, gt){
+grid.search <- function(X, s = NULL, gt){
+  
+  start.time <- proc.time()
   
   no.cores = round(detectCores()*0.75)
   clus = makeCluster(spec = no.cores, type = 'PSOCK')
   registerDoParallel(clus)
   
   N <- nrow(X)
-  d <- ncol(X)
+  # d <- ncol(X)
   mat <- matrix(0, N, N)
   
   for(i in 1:N){
@@ -20,8 +20,7 @@ grid.search <- function(X, s, gt){
   }
   
   mat2 <- c()
-  for(l in 1:N)
-  {
+  for(l in 1:N){
     mat2[l] <- sum(X[l,]^2)
   }
   
@@ -30,29 +29,44 @@ grid.search <- function(X, s, gt){
   
   low <- min(dist.mat)
   high <- max(mat2)
-  
-  c.int <- (high - low) / 999
-
-  print("HI")
+  print(c(low, high))
+  c.int <- (high - low) / 99
   
   seq.lambda <- seq(low, high, by = c.int)
   
-  clusterExport(clus, c('X','s','gt','sparse.dpm.fr.1','asg.func'), 
+  print("HI")
+  
+  # source('~/R/R Codes/Clustering Techniques/dp-means-jrc.R')
+  source('~/R/R Codes/Clustering Techniques/sparse-dpm-fr-1.R')
+  # source('~/R/R Codes/Clustering Techniques/sparse-dpm-fr-2.R')
+  
+  # clusterExport(clus, c('X','s','gt','DP.means'), 
+  #               envir = environment())
+  
+  clusterExport(clus, c('X','s','gt','sparse.dpm.fr.1','asg.func'),
                 envir = environment())
+  
+  # clusterExport(clus, c('X','s','gt','sparse.dpm.fr.2'), 
+  #               envir = environment())
   
   result <- parSapply(clus, seq.lambda, function(lambda.val){
     res <- sparse.dpm.fr.1(X, s, lambda.val, gt)
-    #res <- DP.means(X, i, gt)
-    #res <- sparse.dp.fr(X, s, i, gt)
+    # res <- sparse.dpm.fr.2(X, s, lambda.val, gt)
+    # res <- DP.means(X, lambda.val, gt)
+    
     return(res$NMI)
   })
   
   exec.time <- proc.time() - start.time
   print(exec.time)
   
+  
   stopCluster(clus)
   gc()
   
-  return(list("Maximum NMI" = max(result), 
-              "Corresponding lambda" = seq.lambda[which.max(result)]))
+  final.result <- list("Maximum NMI" = max(result), 
+                       "Corresponding lambda" = seq.lambda[which.max(result)])
+  
+  print(final.result)
+  return(final.result)
 }
