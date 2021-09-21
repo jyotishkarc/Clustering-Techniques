c9bd9d135916857cad6e9e0d5f303c1a59e2e9d7
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-09-22 02:46:15 +0530

    For loop to iterate any algo 30 times

---
+++ Calculation SKFR.R
@@ -0,0 +1,15 @@
+skfr.cal<-function(X,k,s,gt)
+{
+   v=c()
+   for(i in 1:30){
+      print(i)
+      #v[i]=sparse.km.fr.1(X,k,s,t(initial(t(X),k)),gt)$NMI
+      #v[i]=sparse.km.fr(X,k,s,gt)$NMI
+      v[i]=NMI(KMeansSparseCluster(X,k,
+                                   KMeansSparseCluster.permute(X,k,20)$bestw)[[1]]$Cs,gt)
+      #v[i]=km.pp(X,k,gt)$NMI
+   }
+   
+   print("HI")
+   return(mean(v))
+}
