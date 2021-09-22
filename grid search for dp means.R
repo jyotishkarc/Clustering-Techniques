1d1221c0574786dc9625c753ca35ceacdbb02300
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-09-23 03:19:49 +0530

    Minor edit

--- grid search for dp means.R
+++ grid search for dp means.R
@@ -33,8 +33,8 @@ grid.search <- function(X, s, gt)
     #print("yay")
     
     #res[[t]] <- DP.means(X, i, gt)
-    res[[t]] <- sparse.dpm.fr.1(X, s, i, gt)
-    #res[[t]] <- sparse.dp.fr(X,s,i,gt)
+    #res[[t]] <- sparse.dpm.fr.1(X, s, i, gt)
+    res[[t]] <- sparse.dpm.fr.2(X,s,i,gt)
     
     #print(i)
     
