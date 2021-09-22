17cdbb6ccb156c8bd0175bcff7b2d706512f28eb
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-09-23 03:19:13 +0530

    Minor edit

--- Calculation SKFR.R
+++ Calculation SKFR.R
@@ -3,10 +3,14 @@ skfr.cal<-function(X,k,s,gt)
    v=c()
    for(i in 1:30){
       print(i)
+      
       #v[i]=sparse.km.fr.1(X,k,s,t(initial(t(X),k)),gt)$NMI
-      #v[i]=sparse.km.fr(X,k,s,gt)$NMI
+      
+      #v[i]=sparse.km.fr.2(X,k,s,gt)$NMI
+      
       v[i]=NMI(KMeansSparseCluster(X,k,
-                                   KMeansSparseCluster.permute(X,k,20)$bestw)[[1]]$Cs,gt)
+                         KMeansSparseCluster.permute(X,k,20)$bestw)[[1]]$Cs,gt)
+      
       #v[i]=km.pp(X,k,gt)$NMI
    }
    
