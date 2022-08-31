98f6cc2ab81ad49075a40c026ad66d3ff836184d
Supratik Basu <supratikbasu18@gmail.com>
2022-08-31 12:20:38 +0530

    Update EW-DP-MoM.R

--- EW-DP-MoM/EW-DP-MoM.R
+++ EW-DP-MoM/EW-DP-MoM.R
@@ -1,9 +1,10 @@
 
-library(aricode)
 library(gtools)
+library(aricode)
 
-ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = NULL, tol = 1e-03){
-  
+
+ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max, ground = NULL, tol = 1e-03)
+{
   n = nrow(X) #Initialization of the Data
   p = ncol(X)
   K = floor(n / L)
@@ -15,7 +16,7 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
   centroid = t(colMeans(X))
   G = list()
   
-  B <- list() #Partitioning of the data into buckets
+  B = list() #Partitioning of the data into buckets
   indices = permute(1 : n)
   for(i in 1 : L)
   {
@@ -25,19 +26,17 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
       B[[i]] = X[indices[((i - 1) * K + 1 + rem) : (i * K + rem)], ]
   }
   
-  f.old = rep(lambda.k - lambda.w * log(p)) #Computation of initial objective function 
+  f.old = rep((lambda.k - lambda.w * log(p)) / n, L) #Computation of initial objective function 
   for(i in 1 : L)
   {
     for(j in 1 : nrow(B[[i]]))
     {
-      f.old[i] = f.old[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[1, ]) %*% 
-                                                               W %*% (B[[i]][j, ] - centroid[1, ]))
+      f.old[i] = f.old[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[1, ]) %*% W %*% (B[[i]][j, ] - centroid[1, ]))
     }
   }
   fun.old = median(f.old)
-  
+  print(f.old)
   t = 1
-  
   while(t <= T.max)
   {
     G[[t]] <- matrix(0, n, p)
@@ -53,6 +52,78 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
         centroid = rbind(centroid, X[i, ])
         C = C + 1
         Z[i] = C
+        for(x in 1 : 3)
+        {
+          f.new = rep((lambda.k * C + lambda.w * sum(w * log(w))), L)
+          if(rem > 0){
+            for(i in 1 : rem)
+            {
+              for(j in 1 : nrow(B[[i]]))
+              {
+                for(k in 1 : C)
+                {
+                  if(Z[indices[(i - 1) * (K + 1) + j]] == k)
+                    f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+                }
+              }
+            }}
+          for(i in (rem + 1) : L)
+          {
+            for(j in 1 : nrow(B[[i]]))
+            {
+              for(k in 1 : C)
+              {
+                if(Z[indices[(rem) * (K + 1) + (i - rem - 1) * K + j]] == k)
+                  f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+              }
+            }
+          }
+          #print(f.new)
+          L.t = which(f.new == median(f.new))
+          grad <- matrix(0, C, p)
+          dist <- matrix(0, nrow(B[[L.t]]), C)
+          for(j in 1 : nrow(B[[L.t]]))
+          {
+            for(l in 1 : C)
+            {
+              dist[j, l] <- sum(w * (B[[L.t]][j, ] - centroid[l, ]) ^ 2)
+            }
+          }
+          D = rep(0, p)
+          for(i in 1 : C)
+          {
+            for(j in 1 : nrow(B[[L.t]]))
+            {
+              if(which.min(dist[j, ])==i)
+              {
+                grad[i, ] <- grad[i, ] + 2 * (centroid[i, ] - B[[L.t]][j, ])
+                D = D + (centroid[i, ] - B[[L.t]][j, ]) ^ 2
+              }
+            }
+            grad[i, ] <- grad[i, ] / nrow(B[[L.t]])
+            # centroid[i, ] <- centroid[i, ] - eta * grad[i, ] / sqrt(eps + sum(grad[i, ] ^ 2))
+            #centroid[i, ] <- centroid[i, ] - eta * grad[i, ]
+          }
+          G[[t]][1 : C, ] <- grad
+          gsum <- rep(eps, C)
+          for(i in 1 : C)
+          {
+            for(j in 1 : t)
+            {
+              gsum[i] <- gsum[i] + sum((G[[t]][i, ])^2) 
+            }
+            centroid[i, ] <- centroid[i, ] - eta * grad[i, ] / gsum[i]
+          }
+          for(y in 1 : n)
+          {
+            dist.2 = c()
+            for(a in 1 : C)
+            {
+              dist.2[a] = as.numeric(t(X[y, ] - centroid[a, ]) %*% W %*% (X[y, ] - centroid[a, ]))
+            }
+            Z[y] = which.min(dist.2)
+          } 
+        }
       }
       else
         Z[i] = which.min(dist)
@@ -60,29 +131,30 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
     
     f.new = rep((lambda.k * C + lambda.w * sum(w * log(w))), L)
     if(rem > 0){
-    for(i in 1 : rem)
-    {
-      for(j in 1 : nrow(B[[i]]))
+      for(i in 1 : rem)
       {
-        for(k in 1 : C)
+        for(j in 1 : nrow(B[[i]]))
         {
-          if(Z[indices[(i - 1) * (L + 1) + j]] == k)
-            f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+          for(k in 1 : C)
+          {
+            if(Z[indices[(i - 1) * (K + 1) + j]] == k)
+              f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+          }
         }
-      }
-    }}
+      }}
     for(i in (rem + 1) : L)
     {
       for(j in 1 : nrow(B[[i]]))
       {
         for(k in 1 : C)
         {
-          if(Z[indices[(rem) * (L + 1) + (i - rem - 1) * L + j]] == k)
+          if(Z[indices[(rem) * (K + 1) + (i - rem - 1) * K + j]] == k)
             f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
         }
       }
     }
-    L.t = which.median(f.new)
+    #print(f.new)
+    L.t = which(f.new == median(f.new))
     grad <- matrix(0, C, p)
     dist <- matrix(0, nrow(B[[L.t]]), C)
     for(j in 1 : nrow(B[[L.t]]))
@@ -119,6 +191,31 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
     }
     w = exp(-D / lambda.w)
     w = w / sum(w)
+    f.new = rep((lambda.k * C + lambda.w * sum(w * log(w))) / n, L)
+    if(rem > 0){
+      for(i in 1 : rem)
+      {
+        for(j in 1 : nrow(B[[i]]))
+        {
+          for(k in 1 : C)
+          {
+            if(Z[indices[(i - 1) * (K + 1) + j]] == k)
+              f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+          }
+        }
+      }}
+    print("HI")
+    for(i in (rem + 1) : L)
+    {
+      for(j in 1 : nrow(B[[i]]))
+      {
+        for(k in 1 : C)
+        {
+          if(Z[indices[(rem) * (K + 1) + (i - rem - 1) * K + j]] == k)
+            f.new[i] = f.new[i] + (1 / nrow(B[[i]])) * as.numeric(t(B[[i]][j, ] - centroid[k, ]) %*% W %*% (B[[i]][j, ] - centroid[k, ]))
+        }
+      }
+    }
     fun.new = median(f.new)
     v <- sort(unique(Z))
     u <- Z
@@ -138,18 +235,17 @@ ew.dpm.mom = function(X, lambda.k, lambda.w, eps, L, eta, T.max = 100, ground = 
       break
     fun.old = fun.new
     t = t + 1
+    print(t)
   }
   if(is.null(ground) == F)
   {
     nmi.clus <- aricode::NMI(ground, Z)
     ari.clus <- aricode::ARI(ground, Z)
-    print(C)
-    print(nmi.clus)
-    print(ari.clus)
+    # print(C)
+    # print(nmi.clus)
+    # print(ari.clus)
     return(list("Z" = Z, "C" = C, "NMI" = nmi.clus, "ARI" = ari.clus, 
-                "objective" = fun.new))
+                "objective" = fun.new, "W" = w))
   }
   return(list("Z" = Z, "C" = C, "objective" = fun.new))
 }
-
-
