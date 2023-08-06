6e2972e4b0ad48928998abcf408521d22e34eebc
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-08-06 19:24:30 +0530

    Added the method names

--- friedman-rank-test.R
+++ friedman-rank-test.R
@@ -1,13 +1,17 @@
 
 library(dplyr)
+library(readxl)
 
-res_DPM <- read_excel("D:/All Downloads/DP-MoM Results (1).xlsx")
+res_DPM <- read_excel("D:/All Downloads/DP-MoM Results (4).xlsx")
 
 # rank.all.long <- rank.all %>% gather(key = "methods", value = "dataset", 
 #                                      V2, V3, V4, V5, V6, V7, V8, V9)
 
 friedman <- function(df){
-   rank.mat <- df %>% apply(1, function(vals) rank(desc(vals))) %>% t() %>% as.data.frame()
+   rank.mat <- df %>% 
+                  apply(1, function(vals) rank(desc(vals))) %>% 
+                  t() %>% 
+                  as.data.frame()
    # colnames(rank.mat) <- paste0('V',1:9)
    
    N <- nrow(rank.mat)
@@ -18,12 +22,21 @@ friedman <- function(df){
    return(list(test.stat = Q, pval = 1 - pchisq(Q, df = K-1)))
 }
 
-df.temp <- res_DPM
-colnames(df.temp) <- paste0('V',1:9)
+df.temp <- res_DPM[,-1]
+colnames(df.temp) <- paste0('V',1:10)
 
-# df.temp %>% 
-#    select(-c(V5,V8,V9)) %>% 
-#    apply(1, function(vals) rank(desc(vals))) %>% t() %>% as.data.frame() -> df.temp.1
+
+# 1: K-means++ 
+# 2: SKM
+# 3: K-medians
+# 4: PAM
+# 5: RCC
+# 6: DP-means
+# 7: Kb-MoM
+# 8: MOMPKM
+# 9: OWL-KM
+# 10: DP-MoM
+
 
 friedman(df.temp)
 friedman(df.temp %>% select(-c(V9)))
